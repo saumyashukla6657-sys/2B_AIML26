2e388a4a111a0d2eb0b137f3f43f1e6a69988220
saumyashukla6657-sys <saumyashukla6657@gmail.com>
2026-07-30 12:00:46 +0530

    first commit

---
+++ Day-1/index.js
@@ -0,0 +1,24 @@
+const app = require('./app.js');
+//var a = 20;
+//var b = 30;
+//var c = 40;
+//console.log(a+b+c);
+/*var x = 5;
+if(x%2==0){
+    console.log("x is even");
+}
+else{
+    console.log("x is odd");
+}*/
+/*for(var i =0; i<5; i++){
+    console.log(i);
+}*/
+
+/*var x=20;
+const arr=[1, 2, 3, 4, 5];
+for(var i=0; i<arr.length; i++){
+    console.log(arr[i]);
+    console.log(arr[2]);
+}*/
+console.log(app.xz);
+console.log(app.yy);
