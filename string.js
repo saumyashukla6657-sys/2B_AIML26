40e4430003376eb7ab77f3d9ba6b153a37e5cd0c
saumyashukla6657-sys <saumyashukla6657@gmail.com>
2026-08-18 20:24:40 +0530

    string.js

---
+++ string.js
@@ -0,0 +1,19 @@
+let name = "Saumya";
+let lastName = "Shukla";
+
+console.log("Full Name:", name + " " + lastName);
+
+name = name.toUpperCase();
+console.log("Full Name (Uppercase):", name + " " + lastName);
+lastName = lastName.toUpperCase();
+console.log("Full Name (Uppercase):", name + " " + lastName);
+
+console.log(name.charAt(2));
+console.log(name.indexOf("U"));
+console.log(name.lastIndexOf("A"));
+console.log(name.replace("SAUMYA", "Shivani"));
+console.log(name.split(""));
+console.log(name.slice(1, 4));
+console.log(name.includes("Shukla"));
+
+
