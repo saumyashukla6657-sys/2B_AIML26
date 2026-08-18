9aab5a710998cd0c6d8007b32fd915b4edbdcde9
saumyashukla6657-sys <saumyashukla6657@gmail.com>
2026-08-18 20:28:21 +0530

    date.js

---
+++ date.js
@@ -0,0 +1,21 @@
+let date = new Date();
+
+console.log("Date:", date);
+
+// Get year
+console.log("Year:", date.getFullYear());
+
+// Get month
+console.log("Month:", date.getMonth() + 1);
+
+// Get day
+console.log("Day:", date.getDate());
+
+// Get hours
+console.log("Hours:", date.getHours());
+
+// Get minutes
+console.log("Minutes:", date.getMinutes());
+
+// Get seconds
+console.log("Seconds:", date.getSeconds());
