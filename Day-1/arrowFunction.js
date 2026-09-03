4d79515d3974ae9eb86067ab685594771f1d796b
saumyashukla6657-sys <saumyashukla6657@gmail.com>
2026-09-03 11:33:33 +0530

    arrowFunction

---
+++ Day-1/arrowFunction.js
@@ -0,0 +1,7 @@
+/*nst hello = () => {
+    console.log("Hello World");
+}*/
+const fullName = (firstName, lastName) => {
+    return `My full name is ${firstName} ${lastName}`;
+}       
+console.log(fullName("Saumya", "Shukla"));
