e5efb1605309077ae307b84f6016afdcd3b4a083
saumyashukla6657-sys <saumyashukla6657@gmail.com>
2026-09-03 11:04:42 +0530

    StringTemp

---
+++ Day-1/stringtemp.js
@@ -0,0 +1,8 @@
+let firstName = "Saumya";
+let lastName = "Shukla";
+
+function fullName(firstName, lastName) {
+    return `My full name is ${firstName} ${lastName}`;
+}
+let name =`Hell0, ${firstName} ${lastName}!`;
+console.log(name);
