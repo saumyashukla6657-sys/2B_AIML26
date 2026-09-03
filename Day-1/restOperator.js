b6742ef58d651e1c114540531fcc75e810a74386
saumyashukla6657-sys <saumyashukla6657@gmail.com>
2026-09-03 12:20:03 +0530

    example

--- Day-1/restOperator.js
+++ Day-1/restOperator.js
@@ -1,4 +1,4 @@
-function sum(name, ...args) {
+/*nction sum(name, ...args) {
     let total = 0;  
     for(let i = 0; i < args.length; i++) {
         total += args[i];
@@ -7,4 +7,19 @@ function sum(name, ...args) {
 }   
 sum("Alice", 1, 2, 3, 4, 5); // Output: 15
 sum("Bob", 10, 20, 30); // Output: 60
-console.log(sum("Charlie", 1, 2, 3, 4, 5,100)); // Output: 115
+console.log(sum("Charlie", 1, 2, 3, 4, 5,100)); // Output: 115*/
+
+function sum(firstName, lastName, ...numbers) {
+
+    let total = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
+    }
+
+    console.log("name", firstName, lastName, "total", total);
+}
+
+sum("Alice", "Smith", 1, 2, 3, 4, 5);
+    
+
