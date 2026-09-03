a8c5280342bc9317c186ffc350d7423fe8963cd7
saumyashukla6657-sys <saumyashukla6657@gmail.com>
2026-09-03 11:59:43 +0530

    RestOp

--- Day-1/restOperator.js
+++ Day-1/restOperator.js
@@ -1,10 +1,10 @@
-function sum(...args) {
+function sum(name, ...args) {
     let total = 0;  
     for(let i = 0; i < args.length; i++) {
         total += args[i];
     }
     return total;
 }   
-sum(1, 2, 3, 4, 5); // Output: 15
-sum(10, 20, 30); // Output: 60
-console.log(sum(1, 2, 3, 4, 5));
+sum("Alice", 1, 2, 3, 4, 5); // Output: 15
+sum("Bob", 10, 20, 30); // Output: 60
+console.log(sum("Charlie", 1, 2, 3, 4, 5,100)); // Output: 115
