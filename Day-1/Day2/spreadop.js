ef8e7b00dc7fa1f67b8e91181bb89d86e2f2014a
saumyashukla6657-sys <saumyashukla6657@gmail.com>
2026-09-08 11:33:40 +0530

    spread2op

--- Day-1/Day2/spreadop.js
+++ Day-1/Day2/spreadop.js
@@ -1,9 +1,16 @@
 function total(...values) {
+
     let total = 0;
+
     for (let i = 0; i < values.length; i++) {
         total += values[i];
     }
+
     console.log(total);
-};
-let arr=[10, 20, 30,40, 50];
-total(...arr); // Output: 150
+}
+
+let arr = prompt("Enter numbers separated by space:")
+    .split(" ")
+    .map(Number);
+
+total(...arr);
