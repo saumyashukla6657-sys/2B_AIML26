bfb811eea3e4d49fdba4e848ad6b9306f57885fb
saumyashukla6657-sys <saumyashukla6657@gmail.com>
2026-09-15 11:46:53 +0530

    oops

---
+++ Day-1/oops4.js
@@ -0,0 +1,12 @@
+class student{
+   static fName="Saumya";
+    static lName="Shukla";
+
+    display(){
+        console.log("First Name:", student.fName);
+        console.log("Last Name:", student.lName);
+    }   
+}
+//student.display();
+let s1 = new student();
+s1.display();
